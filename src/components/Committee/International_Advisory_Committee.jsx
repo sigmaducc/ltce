d52fab77e26b69642405aada790b91e64c52e9e4
SHUBHAM MISHRA <60005297+TonyStark0801@users.noreply.github.com>
2023-01-05 16:29:14 +0530

    Update International_Advisory_Committee.jsx

--- src/components/Committee/International_Advisory_Committee.jsx
+++ src/components/Committee/International_Advisory_Committee.jsx
@@ -8,28 +8,25 @@ function International_Advisory_Committee() {
       </h2>
 
       <ol className="committee-list pl-8">
-        <li>
-          Dr. Helge Janicke,School of Science,Edith Cowan University
-          (ECU),Australia.
-        </li>
-        <li>
-          Dr. Abhijit Sunnapwar, M.D., Professor/Clinical and Faculty Physician
-          - The University of Texas Health Science Center at San Antonio, TX,
-          USA.
-        </li>
-        <li>
-          Dr. Joanna Rosak, Szyrocka Czestochowa University of Technology,
-          Faculty of Management Poland.
-        </li>
-        <li>
-          Dr. Omar Alzubi, associate professor, Al-Balqa Applied University
-          (Jordan).
-        </li>
-        <li>Dr. Mehdi gheisari, Islamic Azad University, Tehran, Iran.</li>
-        <li>Dr. Mukesh Mishra, Massey University, New Zealand.</li>
-        <li>
-          Dr Hamid Rabiei, University College Dublin (UCD), Dublin, Ireland
-        </li>
+       <li>Dr.Santosh Walke,National University of Science and Technology,Muscat Oman</li>
+        <li>Dr. Abhijit Sunnapwar, Professor, The University of Texas, TX, USA</li>
+        <li>Dr Joanna Rosak, Szyrocka Czestochowa University of Technology, Poland</li>
+        <li>Dr. Omar Alzubi, Associate Professor, Al-Balqa Applied University, Jordan</li>
+        <li>Dr. Mehdi Gheisari, Islamic Azad University, Tehran, Iran</li>
+        <li>Dr.Ravishankar Dudhe,Manipal Academy of Higher Education ,Dubai</li>
+        <li>Dr.Imdad Rizvi,Electrical Engineering Division Higher colleges of Technology,Sharjah,UAE</li>
+        <li>Dr.Surya Durbha,IITB,Powai,Mumbai </li>
+        <li>Dr. Suresh K. Ukarande, Associate Dean,  University of Mumbai.</li>
+        <li>Dr J W Bakal Pillai HOC College of Engineering and Technology Panvel</li>
+        <li>Dr. Satish Devane. Principal, MVPS's KBTCOE Nashik,</li>
+        <li>Dr U V Kulkarni Professor, SGGSIE&T Nanded</li>
+        <li>Dr. Deepak Garg, Professor and Head, Bennett University, Noida</li>
+        <li>Dr. Nonita Sharma, NIT Jalandhar, Punjab</li>
+        <li>Dr. M.P Singh,Dean Academics,PCE,Nagpur</li>
+        <li>Dr.Vivek Nanoti,Director (Engineering),LTJSS,PCE,Nagpur</li>
+        <li>Dr.S.A.Dhale,Principal, PCE,Nagpur</li>
+        <li>Dr.Nitin Chaudhary,Principal ,Priyadarshini Bhagwati College of Engineering,Nagpur</li>
+        <li>Dr.Abhay Shende,Principal,Priyadarshini J.L.College of Engineering,Nagpur.</li>
       </ol>
     </div>
   );
